1c15bdb90e31823ff926ec24ee914614576a5014
shiroling <contact.shiroling@gmail.com>
2021-10-03 11:49:17 +0200

    working on the fu**** area of triangles

--- r101_InitiationDeveloppement/ada/tp4/triangles/lesTrianglesCommented.adb
+++ r101_InitiationDeveloppement/ada/tp4/triangles/lesTrianglesCommented.adb
@@ -1,7 +1,6 @@
 with Ada.Numerics.Elementary_Functions; use Ada.Numerics.Elementary_Functions;
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Float_Text_IO; use Ada.Float_Text_IO;
---with Traceur; use Traceur;
 
 procedure lesTriangles  is
     result: Float;
@@ -10,17 +9,21 @@ procedure lesTriangles  is
     c1: Float;
     dp1: Float;
 
+    -- fonction retournant le somme de deux nombres donnés.
     function somme(nb1: in Integer; nb2: in Integer; nb3: in Integer) return Integer is
     begin
     return nb3+nb2+nb3;
     end somme;
 
-
+    --retourne la distance euclidienne entre Deux points de coordonées x, y dans le plan.
+    --Produit certaines fois des erreures jsp pourquoi .
     function distEuclid(nb11: in Float ; nb12: in Float ; nb21: in Float ; nb22: in Float ) return Float  is
     begin
-    return sqrt((nb11-nb21) **2 * (nb12-nb22) **2);
+    return sqrt((nb11-nb21)*(nb11-nb21)*(nb12-nb22)*(nb12-nb22));
     end distEuclid;
 
+    --retourne la surface d'un triangle a partie des coordonnées de ses 3 sommets
+    --ne fonctionne desfois pas à cause de la fonction distEuclid.
     function heron(nb11: in Float ; nb12: in Float ; nb21: in Float ; nb22: in Float;  nb31: in Float ; nb32: in Float) return Float  is
     dp: Float;
     a: Float;
